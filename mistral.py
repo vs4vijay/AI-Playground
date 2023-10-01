8df06236cf6292876baf439ea4d481c62249513f
vs4vijay <vs4vijay@gmail.com>
2023-10-02 01:25:22 +0530

    add vicuna

---
+++ mistral.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# pip install vllm
+
+
+python -u -m vllm.entrypoints.openai.api_server --host 0.0.0.0 --model mistralai/Mistral-7B-v0.1
--- vicuna.py
+++ vicuna.py
@@ -7,7 +7,7 @@ model = "Wizard-Vicuna-7B-Uncensored.ggmlv3.q8_0.bin"
 
 print(f'[+] Loading model: {model}')
 
-llm = AutoModelForCausalLM.from_pretrained(f"./models/{model}", model_type="llama", 
+llm = AutoModelForCausalLM.from_pretrained(f"../../{model}", model_type="llama", 
     # config={
     #     'n_gqa': 8,
     #     'max_new_tokens': 512,
