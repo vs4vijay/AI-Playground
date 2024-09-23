1a3ae50b4d250b19d8e7276f6a8cbfac8a311b7e
vs4vijay <vs4vijay@gmail.com>
2024-09-23 22:40:19 +0530

    changes

---
+++ text_to_speech.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# pipx install TTS
+
+# tts --text "Text for TTS" --model_name "tts_models/en/ljspeech/glow-tts" --out_path output/path/speech.wav
+
+# https://github.com/espnet/espnet
+
+# https://github.com/babysor/MockingBird
+# https://github.com/myshell-ai/OpenVoice
